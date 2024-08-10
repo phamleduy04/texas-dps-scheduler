5cf78cd510a82b60761f0e094ff74672b379ffb4
Jaymit <jaymitd123@gmail.com>
2024-08-10 00:26:34 -0500

    add city name

--- src/Client/index.ts
+++ src/Client/index.ts
@@ -109,34 +109,81 @@ class TexasScheduler {
         return response[0].ResponseId;
     }
 
-    public async getAllLocationFromZipCodes(): Promise<AvailableLocationResponse[]> {
+    public async getAllLocation(): Promise<AvailableLocationResponse[]> {
         const zipcodeList = this.config.location.zipCode;
+        const cityName = this.config.location.cityName;
+        const typeId = this.config.personalInfo.typeId || 71;
+
         const finalArray: AvailableLocationResponse[] = [];
-        for (let i = 0; i < zipcodeList.length; i++) {
-            const requestBody: AvailableLocationPayload = {
-                CityName: '',
-                PreferredDay: 0,
-                // 71 is new driver license
-                TypeId: this.config.personalInfo.typeId || 71,
-                ZipCode: zipcodeList[i],
-            };
-            const response: AvailableLocationResponse[] = await this.requestApi('/api/AvailableLocation/', 'POST', requestBody).then(
-                res => res.body.json() as Promise<AvailableLocationResponse[]>,
-            );
-            if (response === null) {
-                log.warn(`No location found for zipcode: ${zipcodeList[i]}`);
-                sleep.setTimeout(2000);
-                continue;
-            } else if (response.length !== 0) log.info(`Found ${response.length} locations for zipcode: ${zipcodeList[i]}`);
-            response.forEach(el => (el.ZipCode = zipcodeList[i]));
+        if (cityName) {
+            const response = await this.getLocationForCity(cityName, typeId);
             finalArray.push(...response);
+        } else {
+            for (const zipCode of zipcodeList) {
+                const response = await this.getLocationForZipCode(zipCode, typeId);
+                finalArray.push(...response);
+            }
+        }
+        return this.filterAndSortLocations(finalArray);
+    }
+
+    private async getLocationForCity(cityName: string, typeId: number): Promise<AvailableLocationResponse[]> {
+        const requestBody: AvailableLocationPayload = {
+            CityName: cityName,
+            PreferredDay: 0,
+            TypeId: typeId,
+            ZipCode: ''
+        };
+
+        const response = await this.fetchLocationData(requestBody);
+        if (response === null) {
+            log.warn(`No location found for city: ${cityName}`);
             sleep.setTimeout(2000);
+            return [];
         }
 
-        return finalArray.sort((a, b) => a.Distance - b.Distance).filter((elem, index) => finalArray.findIndex(obj => obj.Id === elem.Id) === index);
+        if (response.length !== 0) {
+            log.info(`Found ${response.length} locations for City: ${cityName}`);
+        }
+        response.forEach(el => (el.CityName = cityName));
+        return response;
     }
+
+    private async getLocationForZipCode(zipCode: string, typeId: number): Promise<AvailableLocationResponse[]> {
+        const requestBody: AvailableLocationPayload = {
+            CityName: '',
+            PreferredDay: 0,
+            TypeId: typeId,
+            ZipCode: zipCode,
+        };
+
+        const response = await this.fetchLocationData(requestBody);
+        if (response === null) {
+            log.warn(`No location found for zipcode: ${zipCode}`);
+            sleep.setTimeout(2000);
+            return [];
+        }
+
+        if (response.length !== 0) {
+            log.info(`Found ${response.length} locations for zipcode: ${zipCode}`);
+        }
+        response.forEach(el => (el.ZipCode = zipCode));
+        return response;
+    }
+
+    private async fetchLocationData(requestBody: AvailableLocationPayload): Promise<AvailableLocationResponse[]> {
+        return await this.requestApi('/api/AvailableLocation/', 'POST', requestBody)
+            .then(res => res.body.json() as Promise<AvailableLocationResponse[]>);
+    }
+
+    private filterAndSortLocations(locations: AvailableLocationResponse[]): AvailableLocationResponse[] {
+        return locations
+            .sort((a, b) => a.Distance - b.Distance)
+            .filter((elem, index, self) => self.findIndex(obj => obj.Id === elem.Id) === index);
+    }
+
     public async requestAvailableLocation(): Promise<void> {
-        const response = await this.getAllLocationFromZipCodes();
+        const response = await this.getAllLocation();
         if (this.config.location.pickDPSLocation) {
             if (existsSync('././cache/location.json')) {
                 this.availableLocation = JSON.parse(readFileSync('././cache/location.json', 'utf-8'));
@@ -147,8 +194,11 @@ class TexasScheduler {
             const userResponse = await prompts({
                 type: 'multiselect',
                 name: 'location',
-                message: 'Choose DPS location, you can choose multiple location!',
-                choices: response.map(el => ({ title: `${el.Name} - ${el.Address} - ${el.Distance} miles away from ${el.ZipCode}!`, value: el })),
+                message: 'Choose DPS location, you can choose multiple locations!',
+                choices: response.map(el => ({
+                    title: `${el.Name} - ${el.Address} - ${el.Distance} miles away from ${el.ZipCode ? el.ZipCode : el.CityName}!`,
+                    value: el,
+                })),
             });
             if (!userResponse.location || userResponse.location.length === 0) {
                 log.error('You must choose at least one location!');
@@ -173,7 +223,7 @@ class TexasScheduler {
         log.info('Checking Available Location Dates....');
         if (!this.availableLocation) return;
         const getLocationFunctions = this.availableLocation.map(location => () => sleep.setTimeout(5000).then(() => this.getLocationDates(location)));
-        for (;;) {
+        for (; ;) {
             console.log('--------------------------------------------------------------------------------');
             await this.queue.addAll(getLocationFunctions).catch(() => null);
             await sleep.setTimeout(this.config.appSettings.interval);
@@ -231,10 +281,9 @@ class TexasScheduler {
             return Promise.resolve(true);
         }
         log.info(
-            `${location.Name} is not Available in ${
-                locationConfig.sameDay
-                    ? 'the same day'
-                    : `around ${locationConfig.daysAround.start}-${locationConfig.daysAround.end} days from ${this.config.location.daysAround.startDate}!`
+            `${location.Name} is not Available in ${locationConfig.sameDay
+                ? 'the same day'
+                : `around ${locationConfig.daysAround.start}-${locationConfig.daysAround.end} days from ${this.config.location.daysAround.startDate}!`
             } `,
         );
 
--- src/Interfaces/AvailableLocation.ts
+++ src/Interfaces/AvailableLocation.ts
@@ -1,17 +1,34 @@
-// This type request to AvailableLocation endpoints and check available locations
-export interface AvailableLocationPayload {
-    CityName: string;
+// Base interface for the request payload
+interface BaseLocationPayload {
     PreferredDay: number;
     TypeId: number;
-    ZipCode: string;
 }
 
-// The response is more than this but i only use stuff I needed
-export interface AvailableLocationResponse {
+// Payload for when the CityName is provided
+interface CityLocationPayload extends BaseLocationPayload {
+    CityName: string;
+    ZipCode: ""; // Ensures ZipCode is an empty string if CityName is provided
+}
+
+// Payload for when the ZipCode is provided
+interface ZipCodeLocationPayload extends BaseLocationPayload {
+    ZipCode: string;
+    CityName: ""; // Ensures CityName is an empty string if ZipCode is provided
+}
+
+// Union type for the payload that allows either CityName or ZipCode
+export type AvailableLocationPayload = CityLocationPayload | ZipCodeLocationPayload;
+
+// Base interface for the location response
+export interface AvailableLocationBase {
     Id: number;
     Address: string;
     Distance: number;
     Name: string;
     NextAvailableDate: string;
-    ZipCode: string;
 }
+
+// Response type for when the CityName or ZipCode is provided
+export type AvailableLocationResponse = 
+    | (AvailableLocationBase & { ZipCode: string; CityName?: never })
+    | (AvailableLocationBase & { CityName: string; ZipCode?: never });
