01a60dbb088de9a7679a58ff76eb719d7a6ccc7a
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2025-01-21 16:51:39 -0600

    v5.1.3 release 🎉
    fix #192
    get better points while using browser strategy
    remove unused config key

--- src/Browser/index.ts
+++ src/Browser/index.ts
@@ -1,6 +1,4 @@
 import _ from 'lodash';
-import { fakerEN_US as faker } from '@faker-js/faker';
-
 import puppeteer from 'puppeteer-extra';
 import { executablePath, Page } from 'puppeteer';
 // This plugin prevent bot detection
@@ -22,15 +20,25 @@ export const getAuthTokenFromBroswer = async (): Promise<string> => {
         const browser = await puppeteer.launch({
             headless: process.env.HEADLESS?.toLowerCase() == 'false' ? false : 'shell',
             slowMo: 10,
-            defaultViewport: { width:1920, height:1080 },
             args: ['--no-sandbox', '--disable-setuid-sandbox', '--disk-cache-size=0'],
             browser: 'chrome',
             executablePath: executablePath(),
             timeout: 0,
         });
-
         const [page] = await browser.pages();
-        await page.goto('https://public.txdpsscheduler.com/');
+
+        // Randomize viewport size
+        await page.setViewport({
+            width: 1920 + _.random(0, 200),
+            height: 1080 + _.random(0, 200),
+            deviceScaleFactor: 1,
+            hasTouch: false,
+            isLandscape: false,
+            isMobile: false,
+        });
+        await page.setJavaScriptEnabled(true);
+        await page.setDefaultNavigationTimeout(0);
+        await page.goto('https://www.txdpsscheduler.com/');
 
         // English button
         await page.waitForSelector('.container > button');
@@ -45,20 +53,21 @@ export const getAuthTokenFromBroswer = async (): Promise<string> => {
         // Personal infomation form
         await page.waitForSelector('.v-card__text');
 
-        log.info('Scrolling..');
-        // random scroll
-        const scrollAmount = _.random(-500, 500); // Random scroll between -500 and 500 pixels
-        await page.evaluate(amount => window.scrollBy(0, amount), scrollAmount);
-
-        await nodeTimer.setTimeout(Math.random() * 3000 + 500); // Random delay between 500ms and 1500ms
+        await nodeTimer.setTimeout(_.random(500, 1500)); // Random delay between 500ms and 1500ms
 
         log.dev('inputting personal info..');
-        await page.type('.v-input:nth-child(2) input', faker.person.firstName(), { delay: _.random(200, 600) });
-        await page.type('.v-input:nth-child(3) input', faker.person.lastName(), { delay: _.random(200, 600) });
+        await page.type('.v-input:nth-child(2) input', config.personalInfo.firstName, { delay: _.random(200, 600) });
+        await page.type('.v-input:nth-child(3) input', config.personalInfo.lastName, { delay: _.random(200, 600) });
         await page.type('.v-input:nth-child(4) input', config.personalInfo.dob.replaceAll('/', ''), { delay: _.random(200, 600) });
-        await page.type('.v-input:nth-child(5) input', faker.string.numeric({ length: 4 }), { delay: _.random(200, 600) });
+        await page.type('.v-input:nth-child(5) input', config.personalInfo.lastFourSSN, { delay: _.random(200, 600) });
 
         log.dev('input personal info done');
+
+        log.dev('Scrolling..');
+        await page.evaluate(() => {
+            const scrollHeight = document.body.scrollHeight;
+            window.scrollBy(0, scrollHeight - Math.random() * 400);
+        });
 
         await page.setRequestInterception(true);
         log.dev('Request interception enabled');
@@ -66,7 +75,13 @@ export const getAuthTokenFromBroswer = async (): Promise<string> => {
         const captchaTokenPromise = new Promise((resolve, reject) => {
             const timeout = setTimeout(() => reject(new Error('Auth token retrieval timed out after 60 seconds')), 60000);
             // Listen for network requests
-            page.on('request', request => request.continue());
+            page.on('request', request => {
+                if (request.resourceType() == 'stylesheet' || request.resourceType() == 'font' || request.resourceType() == 'image') {
+                    request.abort();
+                } else {
+                    request.continue();
+                }
+            });
 
             page.on('response', response => {
                 log.dev(`Response intercepted: ${response.url()}`);
@@ -88,7 +103,9 @@ export const getAuthTokenFromBroswer = async (): Promise<string> => {
         const tryAgainDialog = async (page: Page, retryTime = 0) => {
             log.dev('google catpcha score too low, trying again!');
             if (retryTime > 10) throw new Error('Captcha token retrieval failled!');
+            nodeTimer.setTimeout(_.random(1000, 3000, false));
             await page.click('.v-dialog--active > div > div > button');
+            nodeTimer.setTimeout(_.random(1000, 3000, false));
             await page.click('.v-card__actions.text-center > button');
             page.waitForSelector('.v-dialog--active')
                 .then(() => setTimeout(() => tryAgainDialog(page, retryTime + 1), 5000))
