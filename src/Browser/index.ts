69741f79f23f514329956396f2f9700a9e4ce760
Duy Pham <32657584+phamleduy04@users.noreply.github.com>
2025-10-13 13:55:16 -0500

    v5.2.0 release 🎉
    - switch to patchright
    - improve detection by force non-headless by default

--- src/Browser/index.ts
+++ src/Browser/index.ts
@@ -1,50 +1,28 @@
 import _ from 'lodash';
-import puppeteer from 'puppeteer-extra';
-import { executablePath, Page } from 'puppeteer';
-// This plugin prevent bot detection
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-// This plugin anonymize user agent
-import AnonymizeUA from 'puppeteer-extra-plugin-anonymize-ua';
+import { chromium, Page } from 'patchright';
 
 import * as log from '../Log';
 import parseConfig from '../Config';
 import nodeTimer from 'node:timers/promises';
 
-puppeteer.use(StealthPlugin());
-puppeteer.use(AnonymizeUA({ stripHeadless: true, makeWindows: true }));
-
 export const getAuthTokenFromBroswer = async (): Promise<string> => {
     const config = parseConfig();
     try {
         // Launch brower instance
-        const browser = await puppeteer.launch({
-            headless: process.env.HEADLESS?.toLowerCase() == 'false' ? false : 'shell',
-            slowMo: 10,
-            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disk-cache-size=0'],
-            executablePath: executablePath(),
-            timeout: 0,
+        const browser = await chromium.launch({
+            channel: 'chrome',
+            headless: false,
         });
-        const [page] = await browser.pages();
+        const page = await browser.newPage();
 
-        // Randomize viewport size
-        await page.setViewport({
-            width: 1920 + _.random(0, 200),
-            height: 1080 + _.random(0, 200),
-            deviceScaleFactor: 1,
-            hasTouch: false,
-            isLandscape: true,
-            isMobile: false,
-        });
+        await page.goto('https://www.dps.texas.gov/section/driver-license/driver-license-services-appointments');
 
-        await page.setJavaScriptEnabled(true);
-        await page.setDefaultNavigationTimeout(0);
-        await page.goto('https://www.txdpsscheduler.com/');
+        await page.waitForSelector('.btn-primary');
+        await page.click('.btn-primary');
 
         // English button
         await page.waitForSelector('.container > button');
         await page.click('.container > button');
-
-        await page.waitForNetworkIdle();
 
         // switch to espanol
         await page.waitForSelector('.v-toolbar__content button:nth-child(2)');
@@ -56,37 +34,32 @@ export const getAuthTokenFromBroswer = async (): Promise<string> => {
         await nodeTimer.setTimeout(_.random(500, 1500)); // Random delay between 500ms and 1500ms
 
         log.dev('inputting personal info..');
-        await page.type('.v-input:nth-child(2) input', config.personalInfo.firstName, { delay: _.random(200, 600) });
-        await page.type('.v-input:nth-child(3) input', config.personalInfo.lastName, { delay: _.random(200, 600) });
-        await page.type('.v-input:nth-child(4) input', config.personalInfo.dob.replaceAll('/', ''), { delay: _.random(200, 600) });
-        await page.type('.v-input:nth-child(5) input', config.personalInfo.lastFourSSN, { delay: _.random(200, 600) });
+        const firstNameInput = await page.locator('.v-input:nth-child(2) input');
+        await firstNameInput.pressSequentially(config.personalInfo.firstName, { delay: _.random(200, 600) });
+        const lastNameInput = await page.locator('.v-input:nth-child(3) input');
+        await lastNameInput.pressSequentially(config.personalInfo.lastName, { delay: _.random(200, 600) });
+        const dobInput = await page.locator('.v-input:nth-child(4) input');
+        await dobInput.pressSequentially(config.personalInfo.dob.replaceAll('/', ''), { delay: _.random(200, 600) });
+        const lastFourSSNInput = await page.locator('.v-input:nth-child(5) input');
+        await lastFourSSNInput.pressSequentially(config.personalInfo.lastFourSSN, { delay: _.random(200, 600) });
 
         log.dev('input personal info done');
-
-        log.dev('Scrolling..');
-        await page.evaluate(() => {
-            const scrollHeight = document.body.scrollHeight;
-            window.scrollBy(0, scrollHeight - Math.random() * 400);
-        });
-
-        log.dev('Setting up network interception...');
-        const client = await page.createCDPSession();
-        await client.send('Network.enable');
 
         const captchaTokenPromise = new Promise((resolve, reject) => {
             const timeout = setTimeout(() => reject(new Error('Auth token retrieval timed out after 60 seconds')), 60000);
 
-            client.on('Network.responseReceived', async event => {
-                if (event.response.url === 'https://apptapi.txdpsscheduler.com/api/auth' && event.response.status === 200) {
-                    const response = await client.send('Network.getResponseBody', { requestId: event.requestId });
+            page.on('response', async response => {
+                if (response.url() === 'https://apptapi.txdpsscheduler.com/api/auth' && response.status() === 200) {
+                    const body = await response.text();
                     clearTimeout(timeout);
-                    resolve(response.body);
+                    resolve(body);
                 }
             });
 
             // Click the login button
             page.waitForSelector('.v-card__actions.text-center > button').then(async () => {
                 await page.click('.v-card__actions.text-center > button');
+                // await page.click('.v-card__actions.text-center > button');
                 page.waitForSelector('.v-dialog--active')
                     .then(() => setTimeout(() => tryAgainDialog(page), 5000))
                     .catch(() => null);
@@ -98,8 +71,10 @@ export const getAuthTokenFromBroswer = async (): Promise<string> => {
             if (retryTime > 10) throw new Error('Captcha token retrieval failled!');
             await nodeTimer.setTimeout(_.random(1000, 3000, false));
             await page.click('.v-dialog--active > div > div > button');
+            // await page.click('.v-dialog--active > div > div > button');
             await nodeTimer.setTimeout(_.random(1000, 3000, false));
             await page.click('.v-card__actions.text-center > button');
+            // await page.click('.v-card__actions.text-center > button');
             page.waitForSelector('.v-dialog--active')
                 .then(() => setTimeout(() => tryAgainDialog(page, retryTime + 1), 5000))
                 .catch(() => null);
@@ -107,6 +82,8 @@ export const getAuthTokenFromBroswer = async (): Promise<string> => {
 
         // Wait for the auth token
         const captchaToken = (await captchaTokenPromise) as string;
+
+        await page.close();
         // Close the browser
         await browser.close();
 
